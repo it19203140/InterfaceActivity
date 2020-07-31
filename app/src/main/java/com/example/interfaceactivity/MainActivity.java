2a81e98d793cf2f6c3a48df728c2513021d1b53f
it19203140 <it19203140@my.sliit.lk>
2020-07-31 19:06:41 +0530

    initial commit - added text box, button, until sendMessage implementation

---
+++ app/src/main/java/com/example/interfaceactivity/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.interfaceactivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /** Called whe the user taps the button **/
+    public void sendMessage (View view) {
+
+    }
+}
