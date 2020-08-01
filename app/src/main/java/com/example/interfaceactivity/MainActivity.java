3344bc5df5a889bc9906392f28dc897cecfa6385
it19203140 <it19203140@my.sliit.lk>
2020-08-01 12:44:53 +0530

    Main sendMessage implementation and DisplayMessageActivity implementataion

--- app/src/main/java/com/example/interfaceactivity/MainActivity.java
+++ app/src/main/java/com/example/interfaceactivity/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.interfaceactivity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +17,9 @@ public class MainActivity extends AppCompatActivity {
 
     /** Called whe the user taps the button **/
     public void sendMessage (View view) {
-
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        Edittext editText = (EditText) findViewById(R.id.editTextTextPersonName);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE,message); startActivity(intent);
     }
 }
